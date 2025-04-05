116481d438058c039d4c93e0beab4f77bb4e0d48
Excius <rao.daksh555@gmail.com>
2025-04-05 12:26:45 +0530

    added user names and deletion, addition from database too

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ import path from "path";
 import http from "http";
 import { fileURLToPath } from "url";
 import { Server } from "socket.io";
+import crypto from "crypto";
+
+// TODO: Add support for nicknames.
+// TODO: Don’t send the same message to the user that sent it. Instead, append the message directly as soon as they press enter.
+// TODO: Add “{user} is typing” functionality.
+// TODO: Show who’s online.
+// TODO: Add private messaging.
+// TODO: Share your improvements!
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -11,27 +19,107 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.urlencoded({ extended: true }));
+
+let users = [];
+
+async function getUserById(id) {
+  return users.find((user) => user.id === id);
+}
+
+async function removeUser(userId) {
+  const userIndex = users.findIndex((user) => {
+    return userId === user.id;
+  });
+
+  console.log("userIndex", userIndex);
+
+  users.splice(userIndex, 1);
+
+  console.log(users);
+}
+
 app.get("/", (req, res) => {
   const filePath = path.join(__dirname, "/../public/index.html");
   res.sendFile(filePath);
 });
 
-io.on("connection", (socket) => {
-  console.log("A user connected");
-  socket.broadcast.emit("hi");
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
+app.post("/", (req, res) => {
+  const { username } = req.body;
+
+  if (!username || typeof username !== "string" || username.trim() === "") {
+    return res
+      .status(400)
+      .send("Username is required and must be a non-empty string");
+  }
+
+  const id = crypto.randomUUID();
+  const user = { username, id };
+  users.push(user);
+  console.log(users);
+  return res.redirect(`/chat?id=${id}`);
+});
+
+app.get("/chat", (req, res) => {
+  const { id } = req.query;
+  const filePath = path.join(__dirname, "/../public/chat.html");
+
+  res.sendFile(filePath);
+});
+
+io.on("connection", async (socket) => {
+  const userId = socket.handshake.query.id;
+  if (!userId) {
+    socket.emit("error", "No user ID provided");
+    socket.disconnect();
+    return;
+  }
+  const user = await getUserById(userId);
+  if (!user) {
+    socket.emit("error", "Invalid user ID");
+    socket.disconnect();
+    return;
+  }
+
+  console.log(user.username, "connected");
+
+  socket.broadcast.emit("chat message", {
+    msg: `${user.username} joined the chat`,
+    name: null,
   });
+
+  socket.on("disconnect", async () => {
+    socket.broadcast.emit("chat message", {
+      msg: `${user.username} left the chat`,
+      name: null,
+    });
+
+    await removeUser(userId);
+
+    console.log(user.username, " disconnected");
+  });
+
   socket.on("chat message", (msg) => {
     io.emit("chat message", msg);
     console.log("Message: " + msg);
   });
+
+  socket.on("user id", async (id) => {
+    const user = await getUserById(id);
+    console.log(user);
+
+    if (user) {
+      socket.emit("user name", user.username);
+    } else {
+      console.log("User not found");
+      socket.emit("user name", "User not found");
+    }
+  });
 });
 
-// io.emit("some event", {
-//   someProperty: "some value",
-//   otherProperty: "other value",
-// });
+app.get("/users", (req, res) => {
+  res.send(users);
+});
 
 server.listen(3000, () => {
   console.log("Server is running on port 3000");
