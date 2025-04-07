08381dec61a2f3e7046b16d4176f7347fc6f07dd
Excius <rao.daksh555@gmail.com>
2025-04-07 12:53:25 +0530

    added dynamic port

--- server/server.js
+++ server/server.js
@@ -125,7 +125,7 @@ io.on("connection", async (socket) => {
 app.get("/users", (req, res) => {
   res.send(users);
 });
-
-server.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
